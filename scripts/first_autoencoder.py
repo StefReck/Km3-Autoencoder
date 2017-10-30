3cba23ea4545aee590ab3196a82ec85c2c023939
Stefan Reck <stefan.reck@fau.de>
2017-10-30 13:03:32 +0100

    run_cnn first_autoencoder implementation progress

--- scripts/first_autoencoder.py
+++ scripts/first_autoencoder.py
@@ -4,7 +4,7 @@ from keras.models import Model
 from keras.layers import Input, Conv3D, UpSampling3D, Conv3DTranspose, AveragePooling3D
 import numpy as np
 import h5py
-
+from run_cnn import *
 
 def setup_conv_model_API():
     #Wie der autoencoder im sequential style, nur mit API
@@ -42,7 +42,7 @@ test_data = "test_muon-CC_and_elec-CC_each_120_xyz_shuffled.h5"
 train_file=data_path+train_data
 #this gets killed, maybe too long
 
-train_file="/home/woody/capn/mppi033h/Data/ORCA_JTE_NEMOWATER/h5_input_projections_3-100GeV/4dTo3d/h5/xyz/JTE_KM3Sim_gseagen_muon-CC_3-100GeV-9_1E7-1bin-3_0gspec_ORCA115_9m_2016_588_xyz.h5"
+train_file="Daten/JTE_KM3Sim_gseagen_muon-CC_3-100GeV-9_1E7-1bin-3_0gspec_ORCA115_9m_2016_588_xyz.h5"
 file=h5py.File(train_file, 'r')
 xyz_hists = np.array(file["x"]).reshape((3498,11,13,18,1))
 
@@ -51,8 +51,10 @@ xyz_hists = np.array(file["x"]).reshape((3498,11,13,18,1))
 autoencoder = setup_conv_model_API()
 autoencoder.compile(optimizer='adam', loss='mse')
 
+train_tuple=(train_file, 3498)
+train_and_test_model(model=autoencoder, modelname="autoencoder_test", train_files=train_tuple, test_files=train_tuple, batchsize=32, n_bins=(11,13,18,1), class_type=None, xs_mean=None, epoch=0,
+                         shuffle=False, lr=None, lr_decay=None, tb_logger=False, swap_4d_channels=None)
+#history = autoencoder.fit(xyz_hists[0:320], xyz_hists[0:320], epochs=1, batch_size=32)
 
-history = autoencoder.fit(xyz_hists, xyz_hists, epochs=100, batch_size=32)
-
-autoencoder.save('/home/woody/capn/mppi013h/Km3-Autoencoder/models/autoencoder_test.h5')
+#autoencoder.save('/home/woody/capn/mppi013h/Km3-Autoencoder/models/autoencoder_test.h5')
 
--- scripts/run_cnn.py
+++ scripts/run_cnn.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+import h5py
+import numpy as np
+
+"""
+train_and_test_model(model, modelname, train_files, test_files, batchsize=32, n_bins=(11,13,18,1), class_type=None, xs_mean=None, epoch=0,
+                         shuffle=False, lr=None, lr_decay=None, tb_logger=False, swap_4d_channels=None):
+"""
 
 def train_and_test_model(model, modelname, train_files, test_files, batchsize, n_bins, class_type, xs_mean, epoch,
                          shuffle, lr, lr_decay, tb_logger, swap_4d_channels):
@@ -8,6 +15,8 @@ def train_and_test_model(model, modelname, train_files, test_files, batchsize, n
     """
     epoch += 1
     fit_model(model, modelname, train_files, test_files, batchsize, n_bins, class_type, xs_mean, epoch, shuffle, swap_4d_channels, n_events=None, tb_logger=tb_logger)
+    #fit_model speichert model ab unter ("models/trained_" + modelname + '_epoch' + str(epoch) + '.h5')
+    #evaluate model evaluated und printet es in der konsole
     evaluate_model(model, test_files, batchsize, n_bins, class_type, xs_mean, swap_4d_channels, n_events=None)
 
     return epoch
@@ -39,9 +48,9 @@ def fit_model(model, modelname, train_files, test_files, batchsize, n_bins, clas
 
     for i, (f, f_size) in enumerate(train_files):  # process all h5 files, full epoch
         if epoch > 1 and shuffle is True: # just for convenience, we don't want to wait before the first epoch each time
-            print 'Shuffling file ', f, ' before training in epoch ', epoch
+            print ('Shuffling file ', f, ' before training in epoch ', epoch)
             shuffle_h5(f, chunking=(True, batchsize), delete_flag=True)
-        print 'Training in epoch', epoch, 'on file ', i, ',', f
+        print ('Training in epoch', epoch, 'on file ', i, ',', f)
 
         if n_events is not None: f_size = n_events  # for testing
 
@@ -49,7 +58,7 @@ def fit_model(model, modelname, train_files, test_files, batchsize, n_bins, clas
             generate_batches_from_hdf5_file(f, batchsize, n_bins, class_type, f_size=f_size, zero_center_image=xs_mean, swap_col=swap_4d_channels),
             steps_per_epoch=int(f_size / batchsize), epochs=1, verbose=1, max_queue_size=10,
             validation_data=validation_data, validation_steps=validation_steps, callbacks=callbacks)
-        model.save("models/trained/trained_" + modelname + '_epoch' + str(epoch) + '.h5') #TODO
+        model.save("models/trained_" + modelname + '_epoch' + str(epoch) + '.h5') #TODO
         
         
         
@@ -66,17 +75,76 @@ def evaluate_model(model, test_files, batchsize, n_bins, class_type, xs_mean, sw
     :param None/int n_events: For testing purposes if not the whole .h5 file should be used for evaluating.
     """
     for i, (f, f_size) in enumerate(test_files):
-        print 'Testing on file ', i, ',', f
+        print ('Testing on file ', i, ',', f)
 
         if n_events is not None: f_size = n_events  # for testing
 
         evaluation = model.evaluate_generator(
             generate_batches_from_hdf5_file(f, batchsize, n_bins, class_type, swap_col=swap_4d_channels, f_size=f_size, zero_center_image=xs_mean),
             steps=int(f_size / batchsize), max_queue_size=10)
-    print 'Test sample results: ' + str(evaluation) + ' (' + str(model.metrics_names) + ')'
+    print ('Test sample results: ' + str(evaluation) + ' (' + str(model.metrics_names) + ')')
         
         
         
+#Copied from utilities/cnn_utilities:
+    
+def generate_batches_from_hdf5_file(filepath, batchsize, n_bins, class_type, f_size=None, zero_center_image=None, yield_mc_info=False, swap_col=None):
+    """
+    Generator that returns batches of images ('xs') and labels ('ys') from a h5 file.
+    :param string filepath: Full filepath of the input h5 file, e.g. '/path/to/file/file.h5'.
+    :param int batchsize: Size of the batches that should be generated. Ideally same as the chunksize in the h5 file.
+    :param tuple n_bins: Number of bins for each dimension (x,y,z,t) in the h5 file.
+    :param (int, str) class_type: Tuple with the umber of output classes and a string identifier to specify the exact output classes.
+                                  I.e. (2, 'muon-CC_to_elec-CC')
+    :param int f_size: Specifies the filesize (#images) of the .h5 file if not the whole .h5 file
+                       but a fraction of it (e.g. 10%) should be used for yielding the xs/ys arrays.
+                       This is important if you run fit_generator(epochs>1) with a filesize (and hence # of steps) that is smaller than the .h5 file.
+    :param ndarray zero_center_image: mean_image of the x dataset used for zero-centering.
+    :param bool yield_mc_info: Specifies if mc-infos (y_values) should be yielded as well.
+                               The mc-infos are used for evaluation after training and testing is finished.
+    :param bool swap_col: Specifies, if the index of the columns for xs should be swapped. Necessary for 3.5D nets.
+                          Currently available: 'yzt-x' -> [3,1,2,0] from [0,1,2,3]
+    :return: tuple output: Yields a tuple which contains a full batch of images and labels (+ mc_info if yield_mc_info=True).
+    """
+    n_bins_x, n_bins_y, n_bins_z, n_bins_t = n_bins[0], n_bins[1], n_bins[2], n_bins[3]
+    #For xyz 3D Data:
+    dimensions = (batchsize, n_bins_x, n_bins_y, n_bins_z, 1)
+
+    while 1:
+        f = h5py.File(filepath, "r")
+        if f_size is None:
+            f_size = len(f['y'])
+            warnings.warn('f_size=None could produce unexpected results if the f_size used in fit_generator(steps=int(f_size / batchsize)) with epochs > 1 '
+                          'is not equal to the f_size of the true .h5 file. Should be ok if you use the tb_callback.')
+
+        n_entries = 0
+        while n_entries <= (f_size - batchsize):
+            # create numpy arrays of input data (features)
+            xs = f['x'][n_entries : n_entries + batchsize]
+            xs = np.reshape(xs, dimensions).astype(np.float32)
+
+            if swap_col is not None:
+                swap_4d_channels_dict = {'yzt-x': [3,1,2,0]}
+                xs[:, swap_4d_channels_dict[swap_col]] = xs[:, [0,1,2,3]]
+
+            if zero_center_image is not None: xs = np.subtract(xs, zero_center_image) # if swap_col is not None, zero_center_image is already swapped
+            # and mc info (labels)
+            y_values = f['y'][n_entries:n_entries+batchsize]
+            y_values = np.reshape(y_values, (batchsize, y_values.shape[1])) #TODO simplify with (y_values, y_values.shape) ?
+            
+            #Erstmal rausgenommen für autoencoder, denn bei mir sind die labels auch xs
+            ys = 0 #np.zeros((batchsize, class_type[0]), dtype=np.float32)
+            # encode the labels such that they are all within the same range (and filter the ones we don't want for now)
+            """
+            for c, y_val in enumerate(y_values): # Could be vectorized with numba, or use dataflow from tensorpack
+                ys[c] = encode_targets(y_val, class_type)
+            """
+            # we have read one more batch from this file
+            n_entries += batchsize
+
+            output = (xs, xs) if yield_mc_info is False else (xs, xs) + (y_values,)
+            yield output
+        f.close() # this line of code is actually not reached if steps=f_size/batchsize
         
         
         
