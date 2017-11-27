9ee80f3c6614051effc8b8c3eb1298fea8a0add3
Stefan Reck <stefan.reck@fau.de>
2017-11-27 14:25:28 +0100

    Added parser

--- compare_saved_models.py
+++ compare_saved_models.py
@@ -5,9 +5,24 @@ Compare weights of two models layer wise.
 
 from keras.models import load_model
 import numpy as np
+import argparse
 
-model_path_1 = "models/vgg_3_small/trained_vgg_3_small_autoencoder_epoch6.h5"
-model_path_2 = "models/vgg_3_small/trained_vgg_3_small_autoencoder_epoch6_supervised_up_down_epoch1.h5"
+
+def parse_input():
+    parser = argparse.ArgumentParser(description='Compare weights of two models layer wise.')
+    parser.add_argument('model_path_1', type=str, help='Model file 1 (.h5 file)')
+    parser.add_argument('model_path_2', type=str, help='Model file 2 (.h5 file)')
+    
+    args = parser.parse_args()
+    params = vars(args)
+    return params
+
+params = parse_input()
+model_path_1 = params["model_path_1"]
+model_path_2 = params["model_path_2"]
+
+#model_path_1 = "models/vgg_3_small/trained_vgg_3_small_autoencoder_epoch6.h5"
+#model_path_2 = "models/vgg_3_small/trained_vgg_3_small_autoencoder_epoch6_supervised_up_down_epoch1.h5"
 
 
 def do_model_check(model_path_1, model_path_2):
@@ -31,4 +46,6 @@ def do_model_check(model_path_1, model_path_2):
             print("\tEqual? ",np.array_equal(fl_w_1, fl_w_2))
             
         print("\n")
-    
+
+do_model_check(model_path_1, model_path_2)
+
