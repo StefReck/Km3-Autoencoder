1c95ff65ce2bdc7f64ecfdbc84eee7f7e4ccf1b0
Stefan Reck <stefan.reck@fau.de>
2017-12-06 11:05:59 +0100

    minor changes

--- plot_statistics.py
+++ plot_statistics.py
@@ -7,13 +7,16 @@ hd = True
 #Test Files to make plots from; Format: vgg_3/trained_vgg_3_autoencoder_test.txt
 #Autoencoders:
 test_files = ["vgg_3/trained_vgg_3_autoencoder_test.txt", 
-              "vgg_3_dropout/trained_vgg_3_dropout_autoencoder_test.txt",
-              "vgg_3_max/trained_vgg_3_max_autoencoder_test.txt",
-              "vgg_3_stride/trained_vgg_3_stride_autoencoder_test.txt",
-              "vgg_3_stride_noRelu/trained_vgg_3_stride_noRelu_autoencoder_test.txt",
-              "vgg_3_eps/trained_vgg_3_eps_autoencoder_test.txt",
-              "vgg_3_small/trained_vgg_3_small_autoencoder_test.txt",
-              "vgg_3_verysmall/trained_vgg_3_verysmall_autoencoder_test.txt",]
+              "vgg_3_reg-e9/trained_vgg_3_reg-e9_autoencoder_test.txt",
+              "vgg_3-eps4/trained_vgg_3-eps4_autoencoder_test.txt",
+              #"vgg_3_dropout/trained_vgg_3_dropout_autoencoder_test.txt",
+              #"vgg_3_max/trained_vgg_3_max_autoencoder_test.txt",
+              #"vgg_3_stride/trained_vgg_3_stride_autoencoder_test.txt",
+              #"vgg_3_stride_noRelu/trained_vgg_3_stride_noRelu_autoencoder_test.txt",
+              "vgg_3-sgdlr01/trained_vgg_3-sgdlr01_autoencoder_test.txt",
+              "vgg_3_eps/trained_vgg_3_eps_autoencoder_test.txt",]
+              #"vgg_3_small/trained_vgg_3_small_autoencoder_test.txt",
+              #"vgg_3_verysmall/trained_vgg_3_verysmall_autoencoder_test.txt",]
 
 #Unfrozen
 xtest_files = ["vgg_3/trained_vgg_3_supervised_up_down_test.txt",
@@ -23,7 +26,7 @@ xtest_files = ["vgg_3/trained_vgg_3_supervised_up_down_test.txt",
               "vgg_3_eps/trained_vgg_3_eps_supervised_up_down_test.txt",]
 
 #Encoders Epoch 10
-test_files = ["vgg_3/trained_vgg_3_autoencoder_epoch10_supervised_up_down_test.txt",
+xtest_files = ["vgg_3/trained_vgg_3_autoencoder_epoch10_supervised_up_down_test.txt",
               "vgg_3_dropout/trained_vgg_3_dropout_autoencoder_epoch10_supervised_up_down_test.txt",
               "vgg_3_max/trained_vgg_3_max_autoencoder_epoch10_supervised_up_down_test.txt",
               "vgg_3_stride/trained_vgg_3_stride_autoencoder_epoch10_supervised_up_down_test.txt", ]
