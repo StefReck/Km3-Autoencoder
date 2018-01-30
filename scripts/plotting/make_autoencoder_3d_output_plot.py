bec08f8a3c6f4d04b1183efbda6d8def96ed0e92
Stefan Reck <stefan.reck@fau.de>
2018-01-30 11:30:56 +0100

    Added additional options

--- scripts/plotting/make_autoencoder_3d_output_plot.py
+++ scripts/plotting/make_autoencoder_3d_output_plot.py
@@ -26,6 +26,9 @@ Saves a single plot for multiple files to the same place where the model is loac
 def parse_input():
     parser = argparse.ArgumentParser(description='Compare original 3d image of an event with the prediciton of an autoencoder. Saves a single plot for multiple files to the same place where the model is loacated at.')
     parser.add_argument('model', type=str, help='The model that does the predictions. Saved plot will be the same except with ending _3d_output_plot.h5')
+    parser.add_argument('how_many', type=str, help='How many plots of events will be in the pdf')
+    parser.add_argument('energy_threshold', type=str, help='Minimum energy of events for them to be considered')
+    
     parser.add_argument('only_data_savename', type=str, default="", nargs="?", help='If given, plot of only data without model comparison of xzt data will be saved to given loation.')
     
     #parser.add_argument('modeltag_lambda_comp', type=str, default=None, nargs="?", help="The modeltag of the model, only if lambda layers are present (which are bugged with load_model). CURRENTLY NOT FUNCTIONAL")
@@ -35,15 +38,12 @@ def parse_input():
     return params
 
 
-#Events to compare, each in a seperate page in the pdf
-which = range(0,10,1)
-
-
-
-debug=0
+debug=False
 if debug==False:
     params = parse_input()
     model_file = params["model"]
+    how_many = params["how_many"]
+    energy_threshold = ["energy_threshold"]
     only_data_savename = params["only_data_savename"]
     
     lambda_comp_model_tag = None #params["modeltag_lambda_comp"]
@@ -70,9 +70,15 @@ else:
     zero_center_file = "../Daten/xzt/train_muon-CC_and_elec-CC_each_240_xzt_shuffled.h5_zero_center_mean.npy"
     plot_file = "test.pdf"
     lambda_comp_model_tag = None
-    which=[5,]
+    #Minimum energy for events to be considered
+    energy_threshold=50
+    which=[0,1,]
     from keras.models import load_model
     compare_histograms=1
+
+
+#Events to compare, each in a seperate page in the pdf
+which = range(0,how_many,1)
 
 
 #minimum number of counts in a bin for it to be displayed in the histogramms
@@ -210,15 +216,26 @@ def make_3d_single_plot(hist_org, n_bins, title, figsize):
     return fig
 
 
-def save_some_plots_to_pdf(autoencoder, file, zero_center_file, which, plot_file, min_counts, n_bins, compare_histograms):
+def save_some_plots_to_pdf(autoencoder, file, zero_center_file, which, plot_file, min_counts, n_bins, compare_histograms, energy_threshold):
     #Only bins with abs. more then min_counts are plotted
     #Open files
     
     zero_center_image = np.load(zero_center_file)
-
-    # event_track: [event_id, particle_type, energy, isCC, bjorkeny, dir_x/y/z, time]
-    labels = file["y"][which]
-    hists = file["x"][which]
+    
+    if energy_threshold > 0:
+        #Only take events with an energy over a certain threshold
+        minimum_energy = energy_threshold #GeV
+        print("Applying threshold of", minimum_energy," GeV; only events with higher energy will be considered")
+        where=file["y"][:, 2]>minimum_energy
+        labels=file["y"][where,:][which,:]
+        hists = file["x"][where,:][which,:]
+        
+    else:
+        #manually select events
+        # event_track: [event_id, particle_type, energy, isCC, bjorkeny, dir_x/y/z, time]
+        print("No energy threshold applied")
+        labels = file["y"][which]
+        hists = file["x"][which]
     
     #Get some hists from the file, and reshape them from eg. 5,11,13,18 to 5,11,13,18,1
     hists=hists.reshape((hists.shape+(1,))).astype(np.float32)
@@ -236,8 +253,38 @@ def save_some_plots_to_pdf(autoencoder, file, zero_center_file, which, plot_file
     
     #Some infos for the title
     ids = labels[:,0].astype(int)
+    particle_type = labels[:,1] #Specifies the particle type, i.e. elec/muon/tau (12, 14, 16). Negative values for antiparticles.
     energies = np.round(labels[:,2],1)
-
+    is_CC = labels[:,3]
+    dir_z = labels[:,7]
+    
+    #Make an array that contains strings Up or down depending on the direction the prt is going
+    up_or_down_array=[]
+    for dz in dir_z:
+        up_or_down = int(np.sign(dz)) # returns -1 if dir_z < 0, 0 if dir_z==0, 1 if dir_z > 0
+        direction = "Up-going" if up_or_down>=0 else "Down-going"
+        up_or_down_array.append(direction)
+        
+    #Make an array that contains eg Muon-CC or Elec-NC
+    pid_array=[]
+    for i in range(len(particle_type)):
+        current_type = "CC" if is_CC[i]==True else "NC"
+        if particle_type[i]<0:
+            particle_type[i]*=-1
+            prefix="Anti-"
+        else:
+            prefix=""
+            
+        if particle_type[i]==12:
+            part="Elec"
+        elif particle_type[i]==14:
+            part="Muon"
+        elif particle_type[i]==16:
+            part="Tau"
+        part=prefix+part
+        
+        pid=part+"-"+ current_type
+        pid_array.append(pid)
 
     #proper layout for xzt:
     figsizes=[(12,7),(6,7)] #[double, single]
@@ -248,17 +295,18 @@ def save_some_plots_to_pdf(autoencoder, file, zero_center_file, which, plot_file
         #pp.attach_note(test_file)
         for i,hist in enumerate(hists):
             if compare_histograms == True:
-                suptitle = "Energy: " + str(energies[i]) + " GeV     Loss: " + str(np.round(losses[i],5))
+                suptitle = pid_array[i]+"\t\t"+up_or_down_array[i]+"\t\t"+"Energy: " + str(energies[i]) + " GeV\t\tLoss: " + str(np.round(losses[i],5))
                 fig = make_3d_plots(reshape_3d_to_3d(hist, min_counts), reshape_3d_to_3d(hists_pred[i], min_counts), n_bins, suptitle, figsizes[0])
             else:
-                suptitle = "Energy: " + str(energies[i]) + " GeV"
+                suptitle = pid_array[i]+"\t\t"+up_or_down_array[i]+"\t\t"+"Energy: " + str(energies[i]) + " GeV"
                 fig = make_3d_single_plot(reshape_3d_to_3d(hist, min_counts), n_bins, suptitle, figsizes[1])
+            print("Saving plot as", plot_file)
             pp.savefig(fig)
             plt.close(fig)
     
 
 
-save_some_plots_to_pdf(autoencoder, test_file, zero_center_file, which, plot_file, min_counts=min_counts, n_bins=n_bins, compare_histograms=compare_histograms)
+save_some_plots_to_pdf(autoencoder, test_file, zero_center_file, which, plot_file, min_counts=min_counts, n_bins=n_bins, compare_histograms=compare_histograms, energy_threshold=energy_threshold)
 
     
     
