aeaf14f2d90a034c8b85a2266b675834d1b47a39
Stefan Reck <stefan.reck@fau.de>
2018-02-12 12:57:04 +0100

    Updated get_dimensions for channel data

--- scripts/util/run_cnn.py
+++ scripts/util/run_cnn.py
@@ -310,47 +310,52 @@ def get_dimensions_encoding(n_bins, batchsize):
                         If a dimension is equal to 1, it means that the dimension should be left out.
     :return: tuple dimensions: 2D, 3D or 4D dimensions tuple (integers).
     """
-    n_bins_x, n_bins_y, n_bins_z, n_bins_t = n_bins[0], n_bins[1], n_bins[2], n_bins[3]
-    if n_bins_x == 1:
-        if n_bins_y == 1:
-            #print 'Using 2D projected data without dimensions x and y'
-            dimensions = (batchsize, n_bins_z, n_bins_t, 1)
-        elif n_bins_z == 1:
-            #print 'Using 2D projected data without dimensions x and z'
-            dimensions = (batchsize, n_bins_y, n_bins_t, 1)
-        elif n_bins_t == 1:
-            #print 'Using 2D projected data without dimensions x and t'
-            dimensions = (batchsize, n_bins_y, n_bins_z, 1)
-        else:
-            #print 'Using 3D projected data without dimension x'
-            dimensions = (batchsize, n_bins_y, n_bins_z, n_bins_t, 1)
-
-    elif n_bins_y == 1:
-        if n_bins_z == 1:
-            #print 'Using 2D projected data without dimensions y and z'
-            dimensions = (batchsize, n_bins_x, n_bins_t, 1)
-        elif n_bins_t == 1:
-            #print 'Using 2D projected data without dimensions y and t'
-            dimensions = (batchsize, n_bins_x, n_bins_z, 1)
-        else:
-            #print 'Using 3D projected data without dimension y'
-            dimensions = (batchsize, n_bins_x, n_bins_z, n_bins_t, 1)
-
-    elif n_bins_z == 1:
-        if n_bins_t == 1:
-            #print 'Using 2D projected data without dimensions z and t'
-            dimensions = (batchsize, n_bins_x, n_bins_y, 1)
-        else:
-            #print 'Using 3D projected data without dimension z'
-            dimensions = (batchsize, n_bins_x, n_bins_y, n_bins_t, 1)
-
-    elif n_bins_t == 1:
-        #print 'Using 3D projected data without dimension t'
-        dimensions = (batchsize, n_bins_x, n_bins_y, n_bins_z, 1)
-
+    if len(n_bins) == 2:
+        #for channel data: n_bins=(31,1)
+        dimensions=(batchsize, n_bins[0], n_bins[1])
     else:
-        #print 'Using full 4D data'
-        dimensions = (batchsize, n_bins_x, n_bins_y, n_bins_z, n_bins_t)
+        #e.g. xzt n_bins=(11,18,50,1)
+        n_bins_x, n_bins_y, n_bins_z, n_bins_t = n_bins[0], n_bins[1], n_bins[2], n_bins[3]
+        if n_bins_x == 1:
+            if n_bins_y == 1:
+                #print 'Using 2D projected data without dimensions x and y'
+                dimensions = (batchsize, n_bins_z, n_bins_t, 1)
+            elif n_bins_z == 1:
+                #print 'Using 2D projected data without dimensions x and z'
+                dimensions = (batchsize, n_bins_y, n_bins_t, 1)
+            elif n_bins_t == 1:
+                #print 'Using 2D projected data without dimensions x and t'
+                dimensions = (batchsize, n_bins_y, n_bins_z, 1)
+            else:
+                #print 'Using 3D projected data without dimension x'
+                dimensions = (batchsize, n_bins_y, n_bins_z, n_bins_t, 1)
+    
+        elif n_bins_y == 1:
+            if n_bins_z == 1:
+                #print 'Using 2D projected data without dimensions y and z'
+                dimensions = (batchsize, n_bins_x, n_bins_t, 1)
+            elif n_bins_t == 1:
+                #print 'Using 2D projected data without dimensions y and t'
+                dimensions = (batchsize, n_bins_x, n_bins_z, 1)
+            else:
+                #print 'Using 3D projected data without dimension y'
+                dimensions = (batchsize, n_bins_x, n_bins_z, n_bins_t, 1)
+    
+        elif n_bins_z == 1:
+            if n_bins_t == 1:
+                #print 'Using 2D projected data without dimensions z and t'
+                dimensions = (batchsize, n_bins_x, n_bins_y, 1)
+            else:
+                #print 'Using 3D projected data without dimension z'
+                dimensions = (batchsize, n_bins_x, n_bins_y, n_bins_t, 1)
+    
+        elif n_bins_t == 1:
+            #print 'Using 3D projected data without dimension t'
+            dimensions = (batchsize, n_bins_x, n_bins_y, n_bins_z, 1)
+    
+        else:
+            #print 'Using full 4D data'
+            dimensions = (batchsize, n_bins_x, n_bins_y, n_bins_z, n_bins_t)
 
     return dimensions
 
