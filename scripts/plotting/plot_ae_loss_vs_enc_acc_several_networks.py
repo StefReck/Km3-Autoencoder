1f9948c4a86e7db143ae2e381406cc7cb89b4232
Stefan Reck <stefan.reck@fau.de>
2018-03-16 11:56:37 +0100

    Additions

--- scripts/plotting/plot_ae_loss_vs_enc_acc_several_networks.py
+++ scripts/plotting/plot_ae_loss_vs_enc_acc_several_networks.py
@@ -5,7 +5,7 @@ Plot of AE loss vs Encoder acc of several networks.
 import numpy as np
 import matplotlib.pyplot as plt
 
-from plot_statistics import make_data_from_files, get_last_prl_epochs, get_default_labels
+from plot_statistics import make_data_from_files, get_last_prl_epochs
 
 test_files_ae = ["models/vgg_5_picture/trained_vgg_5_picture_autoencoder_test.txt",
                  "models/vgg_5_morefilter/trained_vgg_5_morefilter_autoencoder_test.txt",
@@ -31,6 +31,13 @@ how_many_epochs_each_to_train_list = [ [10,]*1+[2,]*5+[1,]*194,
                                        [10,]*1+[2,]*5+[1,]*194,
                                        [10,]*1+[2,]*5+[1,]*194, 
                                        [10,]*1+[2,]*5+[1,]*194,]
+
+label_array_overwrite = ["Autoencoder 600 (picture)",
+                         "Autoencoder 600 (filters)",
+                         "Autoencoder 200",
+                         "Autoencoder 200 (dense)",
+                         "Autoencoder 64",
+                         r"Autoencoder 32 ($\epsilon = 10^{-1}$)",]
 
 
 data_for_plots_ae, ylabel_list_ae, default_label_array_ae = make_data_from_files(test_files_ae)
@@ -64,14 +71,20 @@ for ae_number in range(len(test_files_ae)):
 def make_plot(loss_acc_list, labels):
     fig, ax = plt.subplots(figsize=(10,7))
     for i,model_loss_acc in enumerate(loss_acc_list):
-        ax.plot(model_loss_acc[0], model_loss_acc[1], "o", label=labels[i])
+        ax.plot(model_loss_acc[0], model_loss_acc[1], "o", ms=5, label=labels[i])
     ax.set_xlabel("Autoencoder loss")
     ax.set_ylabel("Encoder accuracy")
     ax.grid()
-    ax.legend(pos="lower left")
+    ax.legend(loc="lower left")
+    fig.suptitle("Autoencoder loss and encoder accuracy\nfor different autoencoder models")
     return fig, ax
 
-fig, ax = make_plot(loss_acc_list, default_label_array_ae)
+if len(label_array_overwrite)==len(default_label_array_ae):
+    label_array = label_array_overwrite
+else:
+    label_array = default_label_array_ae
+
+fig, ax = make_plot(loss_acc_list, label_array)
 plt.show(fig)
 
 
