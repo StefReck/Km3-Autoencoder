1905bf4bb416d9f7776d19090bd438b43ce08dbe
Stefan Reck <stefan.reck@fau.de>
2018-04-27 14:53:40 +0200

    Refined

--- scripts/plotting/plot_ae_loss_vs_enc_acc_several_networks.py
+++ scripts/plotting/plot_ae_loss_vs_enc_acc_several_networks.py
@@ -6,20 +6,37 @@ This is used to show that all the networks get worse at a certain AE loss.
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-sys.path.append('../')
+sys.path.append('/home/woody/capn/mppi013h/Km3-Autoencoder/scripts/')
 
 from plot_statistics import make_data_from_files, get_last_prl_epochs
 from util.saved_setups_for_plot_statistics import get_props_for_plot_parallel, get_how_many_epochs_each_to_train
 
-#tags of the models to plot, as defined in saved_setups_for_plots
-tags=["vgg_5_600_picture","vgg_5_200",
-      "vgg_5_200_dense","vgg_5_64",
-      "vgg_5_32-eps01"]
+#Which plot do
+i_want = "acc"
 
-#labels for the plot
-label_array_overwrite = ["Autoencoder 600 (picture)", "Autoencoder 200",
-                         "Autoencoder 200 (dense)", "Autoencoder 64",
-                         r"Autoencoder 32 ($\epsilon = 10^{-1}$)",]
+def which_plot(do_you_want):
+    #The folder the plots might be saved to
+    base_path = "/home/woody/capn/mppi013h/Km3-Autoencoder/results/plots/loss_vs_ydata/"
+    if do_you_want == "acc": 
+        #tags of the models to plot, as defined in saved_setups_for_plots
+        tags=["vgg_5_600_picture","vgg_5_200",
+              "vgg_5_200_dense","vgg_5_64",
+              "vgg_5_32-eps01"]
+        
+        #Stuff for the plot
+        label_list = ["Autoencoder 600 (picture)", "Autoencoder 200",
+                                 "Autoencoder 200 (dense)", "Autoencoder 64",
+                                 r"Autoencoder 32 ($\epsilon = 10^{-1}$)",]
+        xlabel, ylabel = "Autoencoder loss", "Encoder accuracy"
+        title = "Autoencoder loss and encoder accuracy for different autoencoder models"
+        save_as = "vgg_5_acc.pdf"
+        
+    elif do_you_want=="loss":
+        raise NameError("Nothing is here yet...")
+    
+    save_as=base_path+save_as
+    return tags, label_list,xlabel,ylabel,title,save_as
+
 
 
 def combine_ae_and_parallel(data_ae, data_prl, epoch_schedule):
@@ -47,63 +64,71 @@ def combine_ae_and_parallel(data_ae, data_prl, epoch_schedule):
     return loss_ydata
 
 
-def make_plot(loss_ydata_list, labels):
-    fig, ax = plt.subplots(figsize=(10,7))
+def make_plot(loss_ydata_list, labels, xlabel, ylabel, title):
+    fig, ax = plt.subplots(figsize=(7,7))
     for i,model_loss_ydata in enumerate(loss_ydata_list):
         ax.plot(model_loss_ydata[0], model_loss_ydata[1], "o", ms=5, label=labels[i])
-    ax.set_xlabel("Autoencoder loss")
-    ax.set_ylabel("Encoder accuracy")
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
     ax.grid()
     ax.legend(loc="lower left")
-    fig.suptitle("Autoencoder loss and encoder accuracy for different autoencoder models")
+    fig.suptitle(title)
     return fig
 
 
-
-#Get the names of the _test.txt files (AE and parallel) and the epoch schedule
-#of the models identified with the tags
-test_files_ae, test_files_prl, epoch_schedule_list = [],[],[]
-for tag in tags:
-    #info bundle: [ae_model, prl_model], title, labels_override, save_as, epoch_schedule
-    info_bundle = get_props_for_plot_parallel(tag)
-    ae_file = info_bundle[0][0]
-    prl_file= info_bundle[0][1]
-    epoch_schedule = info_bundle[4]
+def make_the_plot(tags, label_list, xlabel, ylabel, title, save_as):
+    """
+    Main function. Returns the plot.
+    """
+    #Get the names of the _test.txt files (AE and parallel) and the epoch schedule
+    #of the models identified with the tags
+    test_files_ae, test_files_prl, epoch_schedule_list = [],[],[]
+    for tag in tags:
+        #info bundle: [ae_model, prl_model], title, labels_override, save_as, epoch_schedule
+        info_bundle = get_props_for_plot_parallel(tag)
+        ae_file = info_bundle[0][0]
+        prl_file= info_bundle[0][1]
+        epoch_schedule = info_bundle[4]
+        
+        test_files_ae.append(ae_file)
+        test_files_prl.append(prl_file)
+        epoch_schedule_list.append(epoch_schedule)
     
-    test_files_ae.append(ae_file)
-    test_files_prl.append(prl_file)
-    epoch_schedule_list.append(epoch_schedule)
-
-
-#Read out the info from all the files listed above
-data_for_plots_ae, ylabel_list_ae, default_label_array_ae = make_data_from_files(test_files_ae)
-data_for_plots_prl, ylabel_list_prl, default_label_array_prl = make_data_from_files(test_files_prl)
-#data_for_plots:
-# [  [Test_epoch, Test_ydata, Train_epoch, Train_ydata],...    ]
-#                   for every test file, ....
-
-loss_ydata_list = []
-for ae_number in range(len(test_files_ae)):
-    #Go thorugh all the AE-prl doubletts and combine them
-    data_ae        = data_for_plots_ae[ae_number]
-    data_prl       = data_for_plots_prl[ae_number]
-    epoch_schedule = epoch_schedule_list[ae_number]
     
-    loss_ydata = combine_ae_and_parallel(data_ae, data_prl, epoch_schedule)
-    loss_ydata_list.append(loss_ydata)
+    #Read out the info from all the files listed above
+    data_for_plots_ae, ylabel_list_ae, default_label_array_ae = make_data_from_files(test_files_ae)
+    data_for_plots_prl, ylabel_list_prl, default_label_array_prl = make_data_from_files(test_files_prl)
+    #data_for_plots:
+    # [  [Test_epoch, Test_ydata, Train_epoch, Train_ydata],...    ]
+    #                   for every test file, ....
+    
+    loss_ydata_list = []
+    for ae_number in range(len(test_files_ae)):
+        #Go thorugh all the AE-prl doubletts and combine them
+        data_ae        = data_for_plots_ae[ae_number]
+        data_prl       = data_for_plots_prl[ae_number]
+        epoch_schedule = epoch_schedule_list[ae_number]
+        
+        loss_ydata = combine_ae_and_parallel(data_ae, data_prl, epoch_schedule)
+        loss_ydata_list.append(loss_ydata)
+    
+    
+    
+    #Define labels for the plot
+    if len(label_list)==len(default_label_array_ae):
+        label_array = label_list
+    else:
+        label_array = default_label_array_ae
+    
+    fig = make_plot(loss_ydata_list, label_array ,xlabel, ylabel, title)
+    fig.savefig(save_as)
+    print("Plot saved to", save_as)
+    
+    return fig
 
 
-
-#Define labels for the plot
-if len(label_array_overwrite)==len(default_label_array_ae):
-    label_array = label_array_overwrite
-else:
-    label_array = default_label_array_ae
-
-fig = make_plot(loss_ydata_list, label_array)
+fig = make_the_plot(*which_plot(i_want))
 plt.show(fig)
-
-
 
 
     
