8ffe9266f2bbd799179454851fc8004225235dc6
Stefan Reck <stefan.reck@fau.de>
2018-06-11 15:17:56 +0200

    m

--- scripts/plotting/plot_occurance_in_dataset.py
+++ scripts/plotting/plot_occurance_in_dataset.py
@@ -8,20 +8,22 @@ import h5py
 import matplotlib.pyplot as plt
 import argparse
 
+import sys
+sys.path.append('../util/')
+from saved_setups_for_plot_statistics import get_plot_statistics_plot_size
+
 def parse_input():
     parser = argparse.ArgumentParser(description='Plot number of events vs Energy, binned.')
-    parser.add_argument('datafile', nargs="+", type=str, help='Datafile')
+    parser.add_argument('datafile', type=str, help='Datafile')
 
     args = parser.parse_args()
     return  args.datafile
 
-debug=False
+debug=1
 
 if not debug:
     datafile=parse_input()
 
-ylog=True
-xlog=True
 
 if debug:
     energies=np.random.rand(10000)*100
@@ -32,16 +34,29 @@ else:
     #dir_x/y/z -> 5/6/7, time -> 8]
     energies=mc_info[:,2]
     
-if ylog: 
-    plt.yscale("log")
-if xlog:  
-    plt.xscale("log")
-    energy_bins=np.logspace(np.log10(3),np.log10(100),98)
-else:
-    energy_bins=np.linspace(3,100,98)
-plt.hist(energies, energy_bins)
+def make_plot(energies):
+    ylog=True
+    xlog=True
+    
+    figsize, fontsize = get_plot_statistics_plot_size("two_in_one_line")
+    plt.rcParams.update({'font.size': fontsize})
+    
+    fig, ax = plt.subplots(figsize=figsize)
+    if ylog: 
+        plt.yscale("log")
+    if xlog:  
+        plt.xscale("log")
+        energy_bins=np.logspace(np.log10(3),np.log10(100),98)
+    else:
+        energy_bins=np.linspace(3,100,98)
+    ax.hist(energies, energy_bins, histtype="step", lw=2)
+    plt.grid()
+    plt.xlabel("Energy (GeV)")
+    plt.ylabel("Number of Events")
+    return fig
 
-
+fig = make_plot(energies)
+plt.show(fig)
 
 
 
