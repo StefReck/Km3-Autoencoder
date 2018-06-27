a1bfc25ac515456eae453f7fec081d6bdd9bbab4
Stefan Reck <stefan.reck@fau.de>
2018-06-27 13:53:51 +0200

    add

--- compare_saved_models.py
+++ compare_saved_models.py
@@ -3,7 +3,7 @@
 Compare weights of two models layer wise.
 """
 
-from keras.models import load_model
+from keras.models import load_model, Model
 import numpy as np
 import argparse
 
@@ -26,6 +26,21 @@ model_path_2 = params["model_path_2"]
 #model_path_1 = "models/vgg_3_small/trained_vgg_3_small_autoencoder_epoch6.h5"
 #model_path_2 = "models/vgg_3_small/trained_vgg_3_small_autoencoder_epoch6_supervised_up_down_epoch1.h5"
 
+def print_layer_stats(layer_1, layer_2):
+    print("Layer Names:")
+    print("\t", layer_1.name, "\t", layer_2.name)
+    weights_1 = np.array(layer_1.get_weights())
+    weights_2 = np.array(layer_2.get_weights())
+    print("Shape of weights matrix:")
+    for i,filt_1 in enumerate(weights_1):
+        filt_2=weights_2[i]
+        print("\t", filt_1.shape, "\t", filt_2.shape)  
+        
+        fl_w_1 = filt_1.flatten()
+        fl_w_2 = filt_2.flatten()
+        
+        print("\tEqual? ",np.array_equal(fl_w_1, fl_w_2))
+    print("\n")
 
 def do_model_check(model_path_1, model_path_2):
     custom_objects=get_custom_objects()
@@ -34,21 +49,18 @@ def do_model_check(model_path_1, model_path_2):
     
     for layer_no,layer_1 in enumerate(model_1.layers):
         layer_2 = model_2.layers[layer_no]
-        print("Layer Names:")
-        print("\t", layer_1.name, "\t", layer_2.name)
-        weights_1 = np.array(layer_1.get_weights())
-        weights_2 = np.array(layer_2.get_weights())
-        print("Shape of weights matrix:")
-        for i,filt_1 in enumerate(weights_1):
-            filt_2=weights_2[i]
-            print("\t", filt_1.shape, "\t", filt_2.shape)  
+        
+        if not isinstance(layer_1, Model):
+            print_layer_stats(layer_1, layer_2)
             
-            fl_w_1 = filt_1.flatten()
-            fl_w_2 = filt_2.flatten()
+        else:
+            #layer 1 is a model
+            for sub_num, layer in enumerate(layer_1):
+                layer_2_sub = model_2.layers[sub_num]
+                print_layer_stats(layer, layer_2_sub)
+            break
             
-            print("\tEqual? ",np.array_equal(fl_w_1, fl_w_2))
-            
-        print("\n")
+       
 
 do_model_check(model_path_1, model_path_2)
 
