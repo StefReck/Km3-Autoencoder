65664343d0524183eba55c71fd7ee924d7c83d96
Stefan Reck <stefan.reck@fau.de>
2018-07-10 11:51:06 +0200

    fix

--- scripts/plotting/plot_occurance_in_dataset.py
+++ scripts/plotting/plot_occurance_in_dataset.py
@@ -9,20 +9,20 @@ import h5py
 import matplotlib.pyplot as plt
 
 import sys
-#sys.path.append('scripts/util/')
-sys.path.append('../util/')
+sys.path.append('scripts/util/')
+#sys.path.append('../util/')
 from saved_setups_for_plot_statistics import get_plot_statistics_plot_size
 
 datafile="data/xzt/train_muon-CC_and_elec-CC_each_240_xzt_shuffled.h5"
 save_to="results/plots/stats_train_muon-CC_and_elec-CC_each_240_xzt_shuffled.pdf"
 save_to_down="results/plots/stats_train_muon-CC_and_elec-CC_each_240_xzt_shuffled_down.pdf"
 
-debug=1
+debug=0
 
 
 if debug:
-    energies=np.random.rand(10000)*100
-    downgoing=np.random.randint(0,2,size=10000)
+    energies=np.random.rand(1000)*100
+    downgoing=np.random.randint(0,2,size=1000)
 else:
     file=h5py.File(datafile, "r")
     mc_info=file["y"]
@@ -54,23 +54,28 @@ def make_plot(energies):
     return fig
 
 
-def make_plot_down(downgoing):
+def make_plot_down(downgoing, energy):
     figsize, fontsize = get_plot_statistics_plot_size("two_in_one_line")
     plt.rcParams.update({'font.size': fontsize})
     
     fig, ax = plt.subplots(figsize=figsize)
     energy_bins=np.linspace(3,100,98)
     
-    ax.hist(downgoing, energy_bins, histtype="step", lw=2, density=True)
+    hist_1d_energy = np.histogram(energy, bins=98, range=(3,100)) #häufigkeit von energien
+    hist_1d_energy_correct = np.histogram(energy[downgoing.astype(bool)], bins=98, range=(3,100)) #häufigkeit von richtigen energien
+    hist_1d_energy_accuracy_bins = np.divide(hist_1d_energy_correct[0], hist_1d_energy[0], dtype=np.float32) #rel häufigkeit von richtigen energien
+    
+    print("Total fraction of down going events:", np.mean(downgoing))
+    ax.step(energy_bins, hist_1d_energy_accuracy_bins, lw=2, )
     plt.grid()
     plt.xlabel("Energy (GeV)")
-    plt.ylabel("Fraction of down-going events")
+    plt.ylabel("Down-going (%)")
     plt.subplots_adjust(left=0.2)
     return fig
 
 
-#fig = make_plot(energies)
-fig_down = make_plot_down(downgoing)
+fig = make_plot(energies)
+fig_down = make_plot_down(downgoing, energies)
 
 plt.show(fig_down)
 
